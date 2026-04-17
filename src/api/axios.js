66559b593edd7e8916844e158933549af95b403c
zero-is-me <zero.is.me19@gmail.com>
2026-04-17 12:57:27 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { authStorage } from "../store/authStorage";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
